3dd5d25eac823cc941167f9868c1448e13fc4a1c
Rharrison3333 <rharri19@smartweb.augustatech.edu>
2026-05-21 16:52:18 -0400

    Update and rename chaos3.py to chaos4.py

--- chaos4.py
+++ chaos4.py
@@ -2,7 +2,7 @@ def main():
     print("This program illustrates a chaotic function")
     x = float(input("Enter a number between 0 and 1: "))
 
-    k = 3.2
+    k = 3.5
 
     for i in range(30):
         x = k * x * (1 - x)
